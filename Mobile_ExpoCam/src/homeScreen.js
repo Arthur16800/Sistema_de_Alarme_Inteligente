88e2928611b01013efe9e48819360b3f90791b73
Arthur16800 <arthur08coutinho@gmail.com>
2025-09-29 08:45:22 -0300

    Adição do mobile

---
+++ Mobile_ExpoCam/src/homeScreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import api from "./axios/axios";
+
+const HomeScreen = () => {
+  const [stateLED, setStateLED] = useState({ value: false });
+  const navigation = useNavigation();
+
+  const handleCam = () => {
+    navigation.navigate("Cam");
+  };
+
+  async function ledToggle() {
+    try {
+      // Faz a requisição da api do adafruit
+      const response = await api.toggleLED({ value: `${!stateLED.value}` });
+      console.log("Resposta: ", response.data);
+      setStateLED({ value: !stateLED.value });
+    } catch (error) {
+      console.log("Erro: ", error.response.data);
+    }
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ marginBottom: 20 }}>
+        <Button title="Abrir Câmera" onPress={handleCam} color="blue" />
+      </View>
+
+      <View style={{ marginBottom: 20 }}>
+        <Button
+          title={stateLED.value ? "Desligar Led" : "Ligar Led"}
+          onPress={ledToggle}
+          color={stateLED.value ? "red" : "green"}
+        />
+      </View>
+
+      <View style={{ marginBottom: 20 }}>
+        <Button
+          title="Listar Eventos"
+          onPress={() => navigation.navigate("Eventos")}
+        />
+      </View>
+    </View>
+  );
+};
+export default HomeScreen;
